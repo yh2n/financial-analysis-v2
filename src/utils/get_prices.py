a8a9ab4fcfb94126485c59ff8147f6b7b15b1efc
Haotian Cai <haotian.cai@nyu.edu>
2020-12-28 01:55:02 -0500

    add building block 2

--- src/utils/get_prices.py
+++ src/utils/get_prices.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 
 
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 
 
 def get_prices(tickers,
@@ -84,10 +84,12 @@ def get_prices_from_yahoo(tickers, start, end, types=None):
     if types is not None:
         df = df[types]
     df = df.apply(print_and_fill_gaps)
-    # send warnings if no data
+    # qc: send warnings if no data
     df.apply(lambda i: print("WARNING: ", i.name,
                              "has no data during the selected period!")
              if i.isna().all() else None)
+    # cast index dtype to datetime64[ns]
+    df.index = pd.to_datetime(df.index)
     return df
 
 
