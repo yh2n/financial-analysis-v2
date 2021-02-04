5777faf6fc2fbbf74c59a1e197a6fa65c926639b
mhanders <6074188+mhanders@users.noreply.github.com>
2021-02-04 19:51:17 +0000

    add holdout strategy

--- src/strats/threshold_momentum.py
+++ src/strats/threshold_momentum.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import numpy as np
 
 
@@ -6,13 +7,14 @@ def threshold_momentum_returns(close_prices, hi_prices, threshold):
     Returns a series with returns on the days the strategy would
     have sold.
 
-    Specifically:
+    Logic:
         - Buy at close_t if return on close from t-1 to t exceeds `threshold`
         - Sell at the high the following day (theoretical)
     """
     close_to_close = close_prices.pct_change()
     close_to_hi = (hi_prices - close_prices.shift()) / close_prices.shift()
-    return close_to_hi[close_to_close.shift() >= threshold]
+    bought_day_before = close_to_close.shift() >= threshold
+    return close_to_hi.where(bought_day_before, other=np.nan)
 
 
 def threshold_momentum_limit_returns(
@@ -21,33 +23,68 @@ def threshold_momentum_limit_returns(
     :returns: a series with the returns on the days the strategy would
     have sold.
 
-    Specifically,
+    Logic:
         - Buy at close_t if return on close from t-1 to t exceeds `threshold`
         - Sell at either
          - a return of `limit`
-         - the high the following day, if lower than `limit`.
+         - the close the following day, if the return never reaches `limit`.
     """
 
     close_to_close = close_prices.pct_change()
     close_to_hi = (hi_prices - close_prices.shift()) / close_prices.shift()
 
-    max_returns = close_to_hi[close_to_close.shift() >= threshold]
-    return np.minimum(limit, max_returns)
+    holding = close_to_close.shift() >= threshold
+    sold_at_limit = (close_to_hi >= limit) & holding
+    sold_at_close = (close_to_hi < limit) & holding
+
+    returns = close_to_close.copy() * np.nan
+    returns[sold_at_limit] = limit
+    returns[sold_at_close] = close_to_close
+
+    return returns
 
 
-def calculate_stats(returns, threshold):
-    thres_data = []
+def threshold_momentum_holdout_returns(
+        close_prices, hi_prices, threshold, limit):
+    """
+    :returns: a series with the returns on the days the strategy would
+    have sold.
 
-    for tick in returns.columns:
-        all_moves = returns[tick].dropna()
-        thres_mean = all_moves.mean()
-        thres_std = all_moves.std()
-        thres_count = all_moves.count()
-        thres_sharpe = thres_mean / thres_std
-        thres_data.append({
-            'ticker': tick, 'mean': thres_mean, 'threshold': threshold,
-            'sharpe': thres_sharpe, 'count': thres_count,
-            'count_pos': (all_moves > 0).sum(),
-            'count_neg': (all_moves < 0).sum(),
-            'std': thres_std})
-    return thres_data
+    Logic:
+        - Buy at close_t if return on close from t-1 to t exceeds `threshold`
+        - Sell at either
+         - a return of `limit`
+         - the close the following day, if the return never reaches `limit`.
+    """
+
+    close_to_close = close_prices.pct_change()
+
+    returns = pd.DataFrame(
+        np.nan, index=close_to_close.index, columns=close_to_close.columns)
+    drawdowns = pd.DataFrame(
+        np.nan, index=close_to_close.index, columns=close_to_close.columns)
+
+    for ticker in returns.columns:
+        holding = False
+        bought_price = None
+
+        closes = close_prices[ticker]
+        his = hi_prices[ticker]
+
+        for day in returns.index[1:]:
+            if holding:  # Check if should sell
+                close_return = (closes.loc[day] - bought_price) / bought_price
+                hi_return = (his.loc[day] - bought_price) / bought_price
+
+                if hi_return >= limit or close_return >= 0:
+                    returns.loc[day, ticker] = (
+                        limit if hi_return >= limit else close_return)
+                    holding = False
+                    bought_price = None
+                else:
+                    drawdowns.loc[day, ticker] = close_return
+            if close_to_close.loc[day, ticker] > threshold:
+                holding = True
+                bought_price = closes.loc[day]
+
+    return returns, drawdowns
