04444648ae5b3a8d8c835eb392caa986ba4a9559
mhanders <6074188+mhanders@users.noreply.github.com>
2021-04-09 13:53:58 -0400

    add docstrings to TW portfolio utils

--- src/strats/tw_portfolio.py
+++ src/strats/tw_portfolio.py
@@ -37,6 +37,27 @@ def n_day_periods(trading_days, offset, n):
 
 
 def corr_to_benchmarks(returns, bench_returns, window):
+    """Calculate the rolling correlation of all stocks in the `returns`
+    time series to each of the benchmarks in the `bench_returns`
+    series.
+
+    The result is in theory a 3D tensor, with dimensions
+        (num_benchmarks) x (num_tickers) x (num_days)
+    but is returned as a dictionary here for simplicity.
+
+    Parameters
+    ----------
+    returns : pd.DataFrame
+    bench_returns : pd.DataFrame
+    window : int
+        Size of the rolling window
+
+    Returns
+    -------
+    dict
+        with keys corresponding to each benchmark, and values the rolling
+        correlations of all tickers in `returns` to that benchmark.
+    """
     corrs = {}
 
     for name, series in bench_returns.items():
@@ -46,6 +67,25 @@ def corr_to_benchmarks(returns, bench_returns, window):
 
 
 def quantile_corr_criteria(corrs1, corrs2, quantile):
+    """Calculates the quantile-based Third Way criteria, which requires that
+    each qualifying stock has a magnitude of correlation to both value and
+    growth based indexes less than some threshold.
+
+    Parameters
+    ----------
+    corrs1 : pd.DataFrame
+        Rolling correlations in time for each ticker to one of the
+        benchmark indexes.
+    corrs2 : pd.DataFrame
+    quantile : int
+
+    Returns
+    -------
+    pd.DataFrame
+        of shape (num_trading_days, num_tickers). Each row is a boolean series
+        of all tickers with truthy values for those satisfying the criteria
+        on that day.
+    """
     cutoffs1 = corrs1.abs().quantile(q=quantile, axis=1)
     cutoffs2 = corrs2.abs().quantile(q=quantile, axis=1)
 
@@ -54,6 +94,21 @@ def quantile_corr_criteria(corrs1, corrs2, quantile):
 
 
 def bottom_k_total_corr_criteria(corrs1, corrs2, k):
+    """Similar signature to `quantile_corr_criteria`, but instead ensures a
+    fixed number of qualifying tickers on each day by computing the maximum
+    correlation magnitude of each ticker to either benchmark, and accepting
+    only those tickers with the `k` lowest scores.
+
+    Parameters
+    ----------
+    corrs1 : pd.DataFrame
+    corrs2 : pd.DataFrame
+    k : int
+
+    Returns
+    -------
+    pd.DataFrame
+    """
     max_corr = corrs1.columns.to_series().apply(
         lambda tick: np.maximum(corrs1.abs()[tick], corrs2.abs()[tick])).T
 
