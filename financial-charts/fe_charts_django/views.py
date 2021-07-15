a904f01d9b58c59676e877bb59c6f904e661d4b7
yh2n <yohann.potico@gmail.com>
2021-07-15 17:49:22 -0400

    Initial commit

---
+++ financial-charts/fe_charts_django/views.py
@@ -0,0 +1,20 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import sys
+sys.path.append('./scripts')
+from chart_seasonality_example import rendering_charts
+
+
+def home_page(request):
+    return render(request, "home.html")
+
+
+def get_new_charts(request):
+    if request.method == 'POST':
+        selected_ticker = request.POST.get('ticker')
+        daily_img_path = "reports/charts/seasonality_daily_{selected_ticker}_2011-01-01_2020-12-31.jpg".format(selected_ticker=selected_ticker)
+        monthly_img_path = "reports/charts/seasonality_monthly_{selected_ticker}_2011-01-01_2020-12-31.jpg".format(selected_ticker=selected_ticker)
+        rendering_charts(selected_ticker)
+        print(f"********** Getting {selected_ticker} charts **********")   
+        return render(request, "home.html", {"daily_img_path": daily_img_path, "monthly_img_path": monthly_img_path})
+    
--- financial-charts/scripts/chart_seasonality_example.py
+++ financial-charts/scripts/chart_seasonality_example.py
@@ -2,23 +2,26 @@ import json
 from datetime import datetime
 from pathlib import Path
 import sys
-sys.path.append('./src')
-from utils.get_prices import get_prices
-from charting.chart_seasonality import (chart_monthly_seasonality,
+sys.path.append('../src')
+from src.utils.get_prices import get_prices
+from src.charting.chart_seasonality import (chart_monthly_seasonality,
                                         chart_cum_avg_daily_rtns)
 
 
-SECRETS_PATH = Path('./tiingo_secrets.json')
-OUTPUT_PATH = Path('./reports/charts')
+SECRETS_PATH = Path('../tiingo_secrets.json')
+OUTPUT_PATH = Path('../financial-charts/static/reports/charts')
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
+def rendering_charts(selected_ticker):
     with open(SECRETS_PATH) as f:
         api_key = json.load(f)['api_key']
     lookback = 10
     today = datetime.today()
     end = f'{today.year - 1}-12-31'
     start = f'{today.year - lookback}-01-01'
-    tk = 'SPY'
+    # tk = 'SPY'
+    tk = selected_ticker
+    print(f"+++++++++++ Ticker: {tk} ++++++++++")
     prices = get_prices([tk], start, end, api_key=api_key)['adj_close'][tk]
     output1_name = f'seasonality_monthly_{tk}_{start}_{end}.jpg'
     output1_path = OUTPUT_PATH / output1_name
