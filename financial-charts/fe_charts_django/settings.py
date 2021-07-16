efa95125ab927503109d5b13332bc811e7b1f00a
yh2n <yohann.potico@gmail.com>
2021-07-16 10:27:20 -0400

    Add heroku app to 'allowed hosts'

--- financial-charts/fe_charts_django/settings.py
+++ financial-charts/fe_charts_django/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-ml-!(9a(@i-d#6(v6(u6(69i96tj2jk7034^a-oqln@13c*8(o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','0.0.0.0']
+ALLOWED_HOSTS = ['127.0.0.1','0.0.0.0', 'financial-charts-django.herokuapp.com']
 
 
 # Application definition
