e9cd2955e40ba4b8663f2765c7bd00f6353e7d99
yh2n <yohann.potico@gmail.com>
2021-07-16 20:02:02 -0400

    Change static file path

--- fe_charts_django/settings.py
+++ fe_charts_django/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ml-!(9a(@i-d#6(v6(u6(69i96tj2jk7034^a-oqln@13c*8(o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','0.0.0.0', 'financial-charts-django.herokuapp.com']
 
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
