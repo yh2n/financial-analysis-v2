d41e281c77f2e4b8ddd252751f69eb645145524c
yh2n <yohann.potico@gmail.com>
2021-07-29 11:01:18 -0400

    Remove used charts

--- fe_charts_django/views.py
+++ fe_charts_django/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 import sys
 sys.path.append('./scripts')
+import io
+
 from chart_seasonality_example import rendering_charts
 
 
@@ -9,11 +11,20 @@ def home_page(request):
     return render(request, "home.html")
 
 
+# def get_new_charts(request):
+#     if request.method == 'POST':
+#         selected_ticker = request.POST.get('ticker')
+#         daily_img_path = "reports/charts/seasonality_daily_{selected_ticker}_2011-01-01_2020-12-31.jpg".format(selected_ticker=selected_ticker)
+#         monthly_img_path = "reports/charts/seasonality_monthly_{selected_ticker}_2011-01-01_2020-12-31.jpg".format(selected_ticker=selected_ticker)
+#         rendering_charts(selected_ticker)
+#         print(f"********** Getting {selected_ticker} charts **********")   
+#         return render(request, "charts.html", {"daily_img_path": daily_img_path, "monthly_img_path": monthly_img_path})
+    
 def get_new_charts(request):
     if request.method == 'POST':
         selected_ticker = request.POST.get('ticker')
-        daily_img_path = "reports/charts/seasonality_daily_{selected_ticker}_2011-01-01_2020-12-31.jpg".format(selected_ticker=selected_ticker)
-        monthly_img_path = "reports/charts/seasonality_monthly_{selected_ticker}_2011-01-01_2020-12-31.jpg".format(selected_ticker=selected_ticker)
+        daily_img_path = "charts/seasonality_daily_{selected_ticker}_2011-01-01_2020-12-31.jpg".format(selected_ticker=selected_ticker)
+        monthly_img_path = "charts/seasonality_monthly_{selected_ticker}_2011-01-01_2020-12-31.jpg".format(selected_ticker=selected_ticker)
         rendering_charts(selected_ticker)
         print(f"********** Getting {selected_ticker} charts **********")   
         return render(request, "charts.html", {"daily_img_path": daily_img_path, "monthly_img_path": monthly_img_path})
--- scripts/chart_seasonality_example.py
+++ scripts/chart_seasonality_example.py
@@ -1,6 +1,7 @@
 import json
 from datetime import datetime
 from pathlib import Path
+import io
 import sys
 sys.path.append('../src')
 from src.utils.get_prices import get_prices
@@ -13,6 +14,26 @@ OUTPUT_PATH = Path('static/reports/charts')
 Path('OUTPUT_PATH').mkdir(parents=True, exist_ok=True)
 
 # if __name__ == '__main__':
+# def rendering_charts(selected_ticker):
+#     with open(SECRETS_PATH) as f:
+#         api_key = json.load(f)['api_key']
+#     lookback = 10
+#     today = datetime.today()
+#     end = f'{today.year - 1}-12-31'
+#     start = f'{today.year - lookback}-01-01'
+#     # tk = 'SPY'
+#     tk = selected_ticker
+#     print(f"+++++++++++ Ticker: {tk} ++++++++++")
+#     prices = get_prices([tk], start, end, api_key=api_key)['adj_close'][tk]
+#     output1_name = f'seasonality_monthly_{tk}_{start}_{end}.jpg'
+#     output1_path = OUTPUT_PATH / output1_name
+#     output2_name = f'seasonality_daily_{tk}_{start}_{end}.jpg'
+#     output2_path = OUTPUT_PATH / output2_name
+#     print("Charting...")
+#     chart_monthly_seasonality(prices, output1_path)
+#     chart_cum_avg_daily_rtns(prices, output2_path)
+#     print("Done!")
+
 def rendering_charts(selected_ticker):
     with open(SECRETS_PATH) as f:
         api_key = json.load(f)['api_key']
@@ -20,7 +41,6 @@ def rendering_charts(selected_ticker):
     today = datetime.today()
     end = f'{today.year - 1}-12-31'
     start = f'{today.year - lookback}-01-01'
-    # tk = 'SPY'
     tk = selected_ticker
     print(f"+++++++++++ Ticker: {tk} ++++++++++")
     prices = get_prices([tk], start, end, api_key=api_key)['adj_close'][tk]
